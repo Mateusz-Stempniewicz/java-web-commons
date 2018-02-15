bdb17971a9dcb6b7aac4ffabec385717625ae56a
Mateusz Stempniewicz <mateusz.stempniewicz@gmail.com>
2018-02-15 07:41:24 +0100

    DELETE annotation

---
+++ java-web-commons/src/main/java/com/java/web/commons/annotation/Delete.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package com.java.web.commons.annotation;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author mateusz.stempniewicz
+ *
+ */
+@Documented
+@Retention(RUNTIME)
+@Target(METHOD)
+@RequestMapping(method = RequestMethod.DELETE)
+public @interface Delete {
+	
+	@AliasFor(annotation = RequestMapping.class)
+	String name() default "";
+	
+	@AliasFor(annotation = RequestMapping.class)
+	String[] value() default {};
+	
+	@AliasFor(annotation = RequestMapping.class)
+	String[] path() default {};
+	
+	@AliasFor(annotation = RequestMapping.class)
+	String[] params() default {};
+	
+	@AliasFor(annotation = RequestMapping.class)
+	String[] headers() default {};
+	
+	@AliasFor(annotation = RequestMapping.class)
+	String[] consumes() default { MediaType.APPLICATION_JSON_VALUE };
+	
+	@AliasFor(annotation = RequestMapping.class)
+	String[] produces() default { MediaType.APPLICATION_JSON_VALUE };
+}
